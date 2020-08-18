7ff970bdd99cd836756915922c6ac36b2d2d6dcf
mhaad <haad1111997@gmail.com>
2020-08-19 00:21:05 +0500

    firestore added

--- lib/model/auth/user.dart
+++ lib/model/auth/user.dart
@@ -1,10 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
   final String uid;
   User({
     this.uid,
   });
 
-  // final CollectionReference userDataCollection = Firestore.instance.collection('UserData');
+  final CollectionReference userDataCollection =
+      Firestore.instance.collection('UserData');
 
-  updateUserData(String name) {}
+  Stream<QuerySnapshot> get data {
+    return userDataCollection.snapshots();
+  }
+
+  Future updateUserData(String name) async {
+    try {
+      return await userDataCollection.document(uid).setData({
+        'name': name,
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 }
